0f07fbe2e2c777aa60298867a2ab26bdb7919a9a
MinhKhoi <mkhoi1776@gmail.com>
2024-02-17 10:17:06 +0700

    adding navigate to header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,12 +1,22 @@
 import "./Header.css";
 import UnimateLogo from "../assets/image/UniMate-Logo-05 1.png";
 import ButtonRedirectOutline from "../components/ButtonRedirectOutline/ButtonRedirectOutline";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const navigateToHome = () => {
+    navigate("");
+  };
+
+  const navigateToBlog = () => {
+    navigate("/blog");
+  };
+
   return (
     <div className="header-section">
       <div className="header-wrapper">
-        <div className="logo-wrapper">
+        <div className="logo-wrapper" onClick={navigateToHome}>
           <img src={UnimateLogo} alt="" />
           <span className="header-title">
             <i>
@@ -16,7 +26,9 @@ const Header = () => {
         </div>
         <div className="menu">
           <div className="menu-item">About</div>
-          <div className="menu-item">Blog</div>
+          <div className="menu-item" onClick={navigateToBlog}>
+            Blog
+          </div>
 
           <div className="menu-item">Contact</div>
           <div className="menu-item">
